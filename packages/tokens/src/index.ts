c99a4e7d375a650f51a2b64172331ded3c3c7c06
Lucas Vilela <fleminglucas@gmail.com>
2023-01-11 13:51:33 +1100

    tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
 export * from "./colors";
+
+export * from "./space";
+export * from "./border-radius";
+
+export * from "./fonts";
+export * from "./font-sizes";
+export * from "./font-weights";
+export * from "./font-weights";
