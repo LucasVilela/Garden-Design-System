a3d3c491610e85328f294fed17055f58f7e4185b
Lucas Vilela <fleminglucas@gmail.com>
2023-01-18 15:02:29 +1100

    Button ds

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,18 +1,73 @@
-import type { StoryObj, Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Button, ButtonProps } from "@garden-ui/react";
+import { ArrowRight } from "phosphor-react";
 
 export default {
-  title: "Button",
+  title: "Form/Button",
   component: Button,
   args: {
-    children: "Button"
+    children: "Send",
+    variant: "primary",
+    size: "md",
+    disabled: false
+  },
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "tertiary"],
+      control: {
+        type: "inline-radio"
+      }
+    },
+    size: {
+      options: ["sm", "md"],
+      control: {
+        type: "inline-radio"
+      }
+    },
+    disabled: {
+      control: {
+        type: "boolean"
+      }
+    },
+    onClick: { action: "clicked" }
   }
-} as Meta;
+} as Meta<ButtonProps>;
 
-export const Primary: StoryObj<ButtonProps> = {
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: "secondary",
+    children: "Create new"
+  }
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: "tertiary",
+    children: "Cancel"
+  }
+};
+
+export const Small: StoryObj<ButtonProps> = {
   args: {
     size: "sm"
   }
 };
 
-export const Secondary: StoryObj<ButtonProps> = {};
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next step
+        <ArrowRight weight="bold" />
+      </>
+    )
+  }
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true
+  }
+};
